9bf4831effe926e3157713d896c9f8d16caae40c
JuanmaPuhl <puhljm@gmail.com>
2019-12-26 21:48:58 -0300

    elimino archivos inutiles

--- compilar.bat
+++ compilar.bat
@@ -1,3 +1,2 @@
-cd C:/Users/juanm/Documents/Juanma/Juego C/Github/C-OpenGLGame
-g++ -o test -I ./librerias/ ./Code/*/*.cpp -L ./librerias/ -lglew32 -lglfw3 -lfreetype -lopengl32
-test
+g++ -o game -I ./librerias/ ./Code/*/*.cpp -L ./librerias/ -lglew32 -lglfw3 -lfreetype -lopengl32
+game
